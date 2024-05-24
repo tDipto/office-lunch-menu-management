9eabfb5b63d2569c388c8e75b689b1c1be2e36ea
Dipto <tdipto@yahoo.com>
2024-05-24 21:40:12 +0600

    feat:primsa admin schema add

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const port = process.env.PORT || 5000;
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
