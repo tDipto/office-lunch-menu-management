04ef69ddd1d24ce0a4443d3c7dc467aa43466f28
Dipto <tdipto@yahoo.com>
2024-05-25 04:28:01 +0600

    fix: admin authorization for menu

--- server/routes/menuRoute.js
+++ server/routes/menuRoute.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const {
+  verifyAdminMiddleware,
+} = require("../middlewares/verifyAdminMiddleware");
+const { verifyAdminToken } = require("../middlewares/verifyAdminToken");
 
 const {
   postMenu,
@@ -7,6 +11,10 @@ const {
   editMenu,
 } = require("../controller/menuController");
 
-router.route("/").post(postMenu).get(getMenuByDate).put(editMenu);
+router
+  .route("/")
+  .post(verifyAdminToken, verifyAdminMiddleware, postMenu)
+  .get(getMenuByDate)
+  .put(verifyAdminToken, verifyAdminMiddleware, editMenu);
 
 module.exports = router;
