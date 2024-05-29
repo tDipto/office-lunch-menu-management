e8fcca208f42aad3abb490f29f032cb7ca848239
Dipto <tdipto@yahoo.com>
2024-05-29 22:01:58 +0600

    fix: design employee list

--- client/src/components/EmployeeList/EmployeeCard/EmployeeCard.jsx
+++ client/src/components/EmployeeList/EmployeeCard/EmployeeCard.jsx
@@ -1,19 +1,67 @@
-import React from "react";
-
+import {
+  ChevronDownIcon,
+  ChevronRightIcon,
+  PresentationChartBarIcon,
+} from "@heroicons/react/24/solid";
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  List,
+  ListItem,
+  ListItemPrefix,
+  Typography,
+} from "@material-tailwind/react";
+import React, { useState } from "react";
 const EmployeeCard = ({ data }) => {
+  const [open, setOpen] = useState(0);
+  // const [openAlert, setOpenAlert] = useState(true);
+
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
   return (
     <div>
-      {/* <h1>Menu card</h1> */}
-      {/* {console.log(data)} */}
-      <div className="p-10">
-        <p>{data.employee.username}</p>
-        <p>Choices:</p>
-        <ul>
-          {data.choices.map((choice) => (
-            <li>{choice}</li>
-          ))}
-        </ul>
-      </div>
+      <List>
+        <Accordion
+          open={open === 1}
+          icon={
+            <ChevronDownIcon
+              strokeWidth={2.5}
+              className={`mx-auto h-4 w-4 transition-transform ${
+                open === 1 ? "rotate-180" : ""
+              }`}
+            />
+          }
+        >
+          <ListItem className="p-0" selected={open === 1}>
+            <AccordionHeader
+              onClick={() => handleOpen(1)}
+              className="border-b-0 p-3"
+            >
+              <ListItemPrefix>
+                <PresentationChartBarIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              <Typography color="blue-gray" className="mr-auto font-normal">
+                {data.employee.username}
+              </Typography>
+            </AccordionHeader>
+          </ListItem>
+          <AccordionBody className="py-1">
+            <List className="p-0">
+              {data.choices.map((choice) => (
+                // <li>{choice}</li>
+                <ListItem>
+                  <ListItemPrefix>
+                    <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                  </ListItemPrefix>
+                  {choice}
+                </ListItem>
+              ))}
+            </List>
+          </AccordionBody>
+        </Accordion>
+      </List>
     </div>
   );
 };
